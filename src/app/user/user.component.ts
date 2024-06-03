188fec7cf4eedac006d8235042ad522c3b9e8db3
SergioFernandez-Mirada <sergio.fernandez@mirada.tv>
2024-06-03 23:35:52 +0200

    First commit with Everything

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomUserIndex];
+
+  // Signals:
+  // selectedUser = signal(DUMMY_USERS[randomUserIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectedUser() {
+    const newRandomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[newRandomIndex];
+
+    // Signals:
+    // this.selectedUser.set(DUMMY_USERS[newRandomIndex]);
+
+  }
+}
